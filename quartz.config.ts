8f3d2014a5076303d4e5c341f81eeb1f89f8148a
DittyEyeroll5 <81958239+DittyEyeroll5@users.noreply.github.com>
2026-01-30 02:24:37 +0800

    Quartz sync: Jan 30, 2026, 2:24 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "dittyeyeroll5.github.io/dittyeyerolldiversions",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
